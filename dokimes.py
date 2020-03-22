32ead203d59b707cc6becb90105e5d0bc67c275d
Argiris888 <59976040+Argiris888@users.noreply.github.com>
2020-03-22 05:38:35 +0200

    ok

---
+++ dokimes.py
@@ -0,0 +1,20 @@
+mystring=' hello world '
+print(mystring.strip())
+print(mystring.index('l',7,12))
+print(mystring.strip().upper())
+my_list_of_words=['ena','duo','tria']
+a_string=' '.join(my_list_of_words)
+print(a_string)
+a_list=a_string.split()
+print(a_list)
+#print(mystring[2:3])
+#a='argiri'
+#print(f'ela spiti {a} ')
+a=3145
+mynewtext=('ela re {}')
+my2newtext=mynewtext.format(a)
+print(my2newtext)
+#mytext=str('einai iso me {:.4f}')
+#print(mytext.format(a))
+#print('einai iso me {:2>5d}'.format(a))
+
