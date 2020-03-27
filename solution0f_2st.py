defe5a6b821017111e29a4f6ffa0e16f0910b27f
Argiris888 <59976040+Argiris888@users.noreply.github.com>
2020-03-27 12:03:43 +0200

    ok

--- solution0f_2st.py
+++ solution0f_2st.py
@@ -5,14 +5,18 @@ maxGrade=100
 minNumberOfEachClass=0
 maxNumberOfEachClass=60
 
+# Size of Class A and random Grades of each individual
 classA_numberOfStudents=np.random.randint( minNumberOfEachClass, maxNumberOfEachClass)
 classA_grades= np.random.randint( minGrade, maxGrade, classA_numberOfStudents)
 
+# Size of Class A and random Grades of each individual
 classB_numberOfStudents=np.random.randint( minNumberOfEachClass, maxNumberOfEachClass)
 classB_grades= np.random.randint( minGrade, maxGrade, classB_numberOfStudents)
 
+# Unique Grades for Both Classes
 uniqueGradesOfBothClasses=set(classA_grades)|set(classB_grades)
 
+# Finding and Counting the Similar Grades Between Class A and Class B
 countOfSimilarGrades=0
 list_of_similarGrades=[]
 for grade in set(classA_grades):
@@ -20,5 +24,5 @@ for grade in set(classA_grades):
         countOfSimilarGrades+=1
         list_of_similarGrades.append(grade)
 
-print(list_of_similarGrades,countOfSimilarGrades)
+print(f'The list of similar grades within Class A and Class B is {list_of_similarGrades} \nTotal number of similar Grades: {countOfSimilarGrades}')
 
